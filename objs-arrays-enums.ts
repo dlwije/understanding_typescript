66313577116c5b54b492c66fc5ef8dd3e9c74cd3
dinesh <dinesh@gdsmc.be>
2021-10-29 11:55:01 +0530

    initial commit of typescript course by mosh

---
+++ objs-arrays-enums.ts
@@ -0,0 +1,36 @@
+//object
+// const person: {
+//     name: string,
+//     age: number,
+//     hobbies: string[],
+//     role: [number, string] //Tuples
+// } = {
+//     name: 'Dinesh',
+//     age:25,
+//     hobbies: ['Sports', 'Cooking'], //array
+//     role: [2, 'auther']
+// };
+
+//enum
+enum Role { ADMIN, READ_ONLY, AUTHER };
+
+const person = {
+    name: 'Dinesh',
+    age:25,
+    hobbies: ['Sports', 'Cooking'], //array
+    role: Role.ADMIN
+};
+
+//array
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
+
+console.log(person.name);
+
+//array for loop
+for(const hobby of person.hobbies){
+    console.log(hobby.toLowerCase());
+
+}
+
+if(person.role === Role.AUTHER) console.log('is auther');
